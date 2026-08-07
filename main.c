9406599e435142ef2277395bd579b15d380f44ec
namekonzu <fumito.2005.0416@gmail.com>
2026-08-08 03:42:03 +0900

    URLに飛べるようにした

--- main.c
+++ main.c
@@ -1,9 +1,104 @@
+#define UNICODE
+#define _UNICODE
+#define ID_TREE 100
 #include <windows.h>
+#include <commctrl.h>
+#include<shellapi.h>
+
+#define ID_BUTTON 1
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
     switch (msg)
     {
+    case WM_CREATE:
+    {
+        HWND tree = CreateWindowW(
+            WC_TREEVIEWW,
+            L"",
+            WS_VISIBLE |
+            WS_CHILD |
+            WS_BORDER |
+            TVS_HASLINES |
+            TVS_LINESATROOT |
+            TVS_HASBUTTONS,
+            20,
+            20,
+            400,
+            500,
+            hwnd,
+            (HMENU)ID_TREE,
+            NULL,
+            NULL
+        );
+        TVINSERTSTRUCTW item = {0};
+
+        item.hParent = TVI_ROOT;
+        item.hInsertAfter = TVI_LAST;
+        item.item.mask = TVIF_TEXT;
+        item.item.pszText = L"青学";
+        
+        HTREEITEM aogaku = TreeView_InsertItem(tree, &item);
+
+        item.hParent = aogaku;
+        item.hInsertAfter = TVI_LAST;
+        item.item.mask = TVIF_TEXT|TVIF_PARAM;
+        item.item.pszText = L"青学ホームページ";
+        item.item.lParam = (LPARAM)L"https://www.aoyama.ac.jp/";
+
+        TreeView_InsertItem(tree, &item);
+    }
+    case WM_COMMAND:
+        if (LOWORD(wParam) == ID_BUTTON)
+        {
+            MessageBoxW(hwnd,
+                L"Hello, World!",
+                L"URLBox",
+                MB_OK
+            );
+        }
+        return 0;
+    case WM_NOTIFY:
+    {
+        LPNMHDR header = (LPNMHDR)lParam;
+        if (header->idFrom == ID_TREE &&
+            header->code == NM_DBLCLK)
+        {
+            HWND tree = header->hwndFrom;
+            HTREEITEM selected =
+                TreeView_GetSelection(tree);
+
+            if (selected != NULL)
+            {
+                TVITEMW selectedItem = {0};
+
+                selectedItem.mask = TVIF_PARAM;
+                selectedItem.hItem = selected;
+
+                TreeView_GetItem(
+                    tree,
+                    &selectedItem
+                );
+
+                if (selectedItem.lParam != 0)
+                {
+                    LPCWSTR url =
+                        (LPCWSTR)selectedItem.lParam;
+
+                    ShellExecuteW(
+                        hwnd,
+                        L"open",
+                        url,
+                        NULL,
+                        NULL,
+                        SW_SHOWNORMAL
+                    );
+                }
+            }
+        }
+
+        return 0;
+    }
     case WM_DESTROY:
         PostQuitMessage(0);
         return 0;
@@ -16,17 +111,25 @@ int WINAPI WinMain(HINSTANCE hInstance,
                    LPSTR lpCmdLine,
                    int nCmdShow)
 {
-    WNDCLASS wc = {0};
+    INITCOMMONCONTROLSEX icex = {0};
+
+    icex.dwSize = sizeof(INITCOMMONCONTROLSEX);
+    icex.dwICC = ICC_TREEVIEW_CLASSES;
+
+    InitCommonControlsEx(&icex);
+
+
+    WNDCLASSW wc = {0};
 
     wc.lpfnWndProc = WndProc;
     wc.hInstance = hInstance;
-    wc.lpszClassName = "MainWindow";
+    wc.lpszClassName = L"MainWindow";
 
-    RegisterClass(&wc);
+    RegisterClassW(&wc);
 
-    HWND hwnd = CreateWindow(
-        "MainWindow",
-        "URLBox",
+    HWND hwnd = CreateWindowW(
+        L"MainWindow",
+        L"URLBox",
         WS_OVERLAPPEDWINDOW,
         CW_USEDEFAULT,
         CW_USEDEFAULT,
